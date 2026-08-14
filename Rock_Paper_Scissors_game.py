6a7a0cbae72f9ab1d7f2e02aad9b3c6e72e48425
Tenesh Chhantyal <tenishchhantyal22@gmail.com>
2026-08-14 20:29:46 +0545

    Implement Rock, Paper, Scissors game

---
+++ Rock_Paper_Scissors_game.py
@@ -0,0 +1,28 @@
+import random
+emojis = {'r':'Rock','s':'scissors','p':'paper'}
+choices = ['r', 'p', 's']
+while True:
+user_choice = input('Rock, paper, or scissors? (r/p/s): ').lower()
+if user_choice not in choices:
+  print("Invalid choice!")
+  continue
+  
+computer_choice = random.choice(choices)
+
+print(f'You chose {emojis[user_choice]}')
+print(f'Computer chose {emojis[computer_choice]}')
+
+if user_choice == computer_choice
+print('Tie!')
+elif (
+  (user_choice == 'r' and computer_choice == 's') or 
+  (user_choice == 's' and computer choice == 'p') or 
+  (user_choice == 'p' and computer_choice == 'r')):
+  print("You win")
+else:
+  print("You lose")
+should_continue = input("Continue? (y/n): ").lower()
+if should_continue == 'n':
+  break
+
+
